72e99ee68a698bbdbe86cced6cc6739c3d07e4c8
hairanndo <hairanndo@gmail.com>
2025-04-27 12:51:32 +0900

    ver.0.1

--- addunderline.bat
+++ addunderline.bat
@@ -1,1 +1,18 @@
-test
+@echo off
+setlocal enabledelayedexpansion
+
+for %%F in (%*) do (
+    set "filepath=%%~fF"
+    set "folder=%%~dpF"
+    set "filename=%%~nF"
+    set "extension=%%~xF"
+
+    rem 新しいファイル名を作成（先頭に "_" を追加）
+    set "newname=_!filename!!extension!"
+
+    rem ファイルをリネーム
+    ren "!filepath!" "!newname!"
+)
+
+endlocal
+exit
